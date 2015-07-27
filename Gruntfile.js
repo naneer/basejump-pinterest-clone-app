71d5a81caf666794db75c946abbb86c595062865
reenan <reenan.arbitrario@gmail.com>
2015-07-27 07:54:23 +0000

    done??

--- Gruntfile.js
+++ Gruntfile.js
@@ -225,13 +225,13 @@ module.exports = function (grunt) {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
         exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ],
-        overrides: {
+/*        overrides: {
           outlayer: {
             main: [
               "item.js", "outlayer.js"
               ]
           }
-        }
+        }*/
         
       },
     },
