7ee6f1b479f9cb1487f42d26ef701370a625aa40
naneer <reenan.arbitrario@hulu.com>
2017-05-01 18:18:02 -0700

    ahh

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -6,9 +6,7 @@ var User = require('./user.model');
 
 var user = new User({
   provider: 'local',
-  name: 'Fake User',
-  email: 'test@test.com',
-  password: 'password'
+  name: 'Fake User'
 });
 
 describe('User Model', function() {
